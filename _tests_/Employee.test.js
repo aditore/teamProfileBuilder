c1762d76615048d2a12364aa8f526122047d467d
aditore <anthonyditore@gmail.com>
2022-02-28 21:09:17 -0800

    finished employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -27,4 +27,40 @@ describe('Employee', () => {
 
         expect(employee.email).toBe(testEmail);
     });
-})
+
+    describe('getName', () => {
+        it('Can get name of Employee via getName()', () => {
+            const name = 'Anthony';
+            const employee = new Employee(name);
+
+            expect(employee.getName()).toBe(name);
+        });
+    });
+
+    describe('getId', () => {
+        it('Can get id of Employee via getId()', () => {
+            const idValue = 6874;
+            const employee = new Employee('Anthony', idValue);
+
+            expect(employee.getId()).toBe(idValue);
+        });
+    });
+
+    describe('getEmail', () => {
+        it('Can get email of Employee via getEmail()', () => {
+            const email = 'test@test.test';
+            const employee = new Employee('Anthony', 6874, email);
+
+            expect(employee.getEmail()).toBe(email);
+        });
+    });
+
+    describe('getRole', () => {
+        it('Can get the role of the Employee via getRole() -should return "Employee"', () => {
+            const role = 'Employee';
+            const employee = new Employee('Anthony', 6874, 'tesst@test.test');
+
+            expect(employee.getRole()).toBe(role);
+        });
+    });
+});
